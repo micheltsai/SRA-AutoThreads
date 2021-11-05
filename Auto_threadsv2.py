d0b6ae6474dcdf48d2869a043798ca95798dd7da
Cai Yi-Xiu <sj985517@gmail.com>
2021-11-05 10:30:01 +0800

    modify

--- Auto_threadsv2.py
+++ Auto_threadsv2.py
@@ -827,8 +827,8 @@ if __name__ == '__main__':
                     for k in need_run:
                         print("########### hello %d ############\n" % prog_num)
                         print("########## {}/{} ###########".format(finish_num, count))
-                        #pool.apply_async(SRA_Analysis, (k,sra_dir,ass_dir,fastq_dir,assemble_dir,))
-                        pool.apply_async(test, (k,new_outdir,))
+                        pool.apply_async(SRA_Analysis, (k,sra_dir,ass_dir,fastq_dir,assemble_dir,))
+                        #pool.apply_async(test, (k,new_outdir,))
                         #progress_list.append(multiprocessing.Process(target=SRA_Analysis, args=(k,)))
                         prog_num += 1
                         finish_num += 1
