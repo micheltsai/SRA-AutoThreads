b90458b3771f71267b852ae0a718eb7e85387b08
Cai Yi-Xiu <sj985517@gmail.com>
2021-11-25 17:36:29 +0800

    modify

--- Auto_threadsv2-3-nchc.py
+++ Auto_threadsv2-3-nchc.py
@@ -910,7 +910,7 @@ if __name__ == '__main__':
                 try:
                     for k in need_run:
                         print("########### hello %d ############\n" % prog_num)
-                        print("########## {}/{} ###########".format(finish_num, count))
+                        print("########## {}/{} ###########".format(finish_num, sra_num_))
                         pool_list.append(pool.apply_async(SRA_Analysis, (k,sra_dir,ass_dir,fastq_dir,assemble_dir,new_outdir,thread,gsize,start,sra_num_,)))
                         #pool.apply_async(test, (k,new_outdir,))
                         #progress_list.append(multiprocessing.Process(target=SRA_Analysis, args=(k,)))
