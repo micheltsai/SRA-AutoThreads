8609bbec1f36de978290c244acd3435ce32d14ad
Cai Yi-Xiu <sj985517@gmail.com>
2021-12-07 14:26:31 +0800

    modify

--- Auto_threadsv2-3-nchc.py
+++ Auto_threadsv2-3-nchc.py
@@ -796,7 +796,7 @@ utils_.mkdir_join(outdir)
 thread = 4
 ##############
 if __name__ == '__main__':
-    pool = multiprocessing.Pool(processes=112)
+    pool = multiprocessing.Pool(processes=56)
     start=time.time()
     #Month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
