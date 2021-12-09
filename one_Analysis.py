bfe870d4a1703f9167f01d4f6326c2bddc03a87b
Cai Yi-Xiu <sj985517@gmail.com>
2021-12-09 09:49:16 +0800

    add one_Analysis(test array job)

--- one_Analysis.py
+++ one_Analysis.py
@@ -747,7 +747,7 @@ if __name__ == '__main__':
     pdat=argvs[1]
     need_run=argvs[2]
     sra_num_ = argvs[3]
-    sra_id=need_run[int(argvs[4])]
+    sra_id=need_run[int(argvs[4])-1]
     sra_id_test=argvs[5]
 
     new_outdir = os.path.join(outdir, pdat)
