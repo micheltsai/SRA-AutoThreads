37df5e6c1ebdd2bccf168a7092c5153503b81ab0
Cai Yi-Xiu <sj985517@gmail.com>
2021-12-09 10:06:45 +0800

    add one_Analysis(test array job)

--- one_Analysis.py
+++ one_Analysis.py
@@ -748,7 +748,7 @@ if __name__ == '__main__':
     need_run=argvs[2]
     sra_num_ = argvs[3]
     sra_id=need_run[int(argvs[4])-1]
-    sra_id_test=argvs[5]
+    #sra_id_test=argvs[5]
 
     new_outdir = os.path.join(outdir, pdat)
     utils_.mkdir_join(new_outdir)
@@ -763,4 +763,9 @@ if __name__ == '__main__':
     assemble_dir = os.path.join(new_outdir, "assembly_result")
     utils_.mkdir_join(assemble_dir)
     print("sra_dir:{}\nass_dir={}\nfastq_dir={}\nassemble_dir={}\n".format(sra_dir, ass_dir, fastq_dir, assemble_dir))
-    SRA_Analysis(sra_id, sra_dir, ass_dir, fastq_dir, assemble_dir, new_outdir, thread, gsize, start, sra_num_)
+    print(need_run)
+    print(need_run.type)
+    print(int(argvs[4])-1)
+    print(sra_id)
+
+    #SRA_Analysis(sra_id, sra_dir, ass_dir, fastq_dir, assemble_dir, new_outdir, thread, gsize, start, sra_num_)
