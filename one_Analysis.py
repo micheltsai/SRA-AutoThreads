918f66bdf999e505150e6bffaf01ea7db79a83f2
Cai Yi-Xiu <sj985517@gmail.com>
2021-12-20 15:01:36 +0800

    download_sbatch add

--- one_Analysis.py
+++ one_Analysis.py
@@ -443,7 +443,16 @@ def Analysis(sra_id,input,target_ref,anoutdir,_outdir,thread,gsize,start):
                     f.write("mlst is ok\n")
             step += 1
         except Exception as e:
-            print(e)
+            error_class = e.__class__.__name__  # 取得錯誤類型
+            detail = e.args[0]  # 取得詳細內容
+            cl, exc, tb = sys.exc_info()  # 取得Call Stack
+            lastCallStack = traceback.extract_tb(tb)[-1]  # 取得Call Stack的最後一筆資料
+            fileName = lastCallStack[0]  # 取得發生的檔案名稱
+            lineNum = lastCallStack[1]  # 取得發生的行號
+            funcName = lastCallStack[2]  # 取得發生的函數名稱
+            errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class,
+                                                                   detail)
+            print(errMsg)
             print("mlst err\n")
             with open("./err_need_again.txt","a+") as f1:
                 f1.write(sra_id)
