e650dadd56b619eb41262d8c9f730b65bcf7a7d4
Cai Yi-Xiu <sj985517@gmail.com>
2022-01-27 11:12:11 +0800

    modify Download_sbatch.py

--- utils_.py
+++ utils_.py
@@ -519,12 +519,23 @@ def run_for_114v3(sra_id,sra_dir,fastq_dir,assemble_dir,outdir,threads,gsize,sta
     #run_cmd("du ./SRAtest -sh")
     #time.sleep(1)
     ###
-
-    print('Dump fastq.\n')
-    # run_cmd
-    dump_fastq_from_sra(path_, fastq_dir)
-    # os.listdir(fastq_dir) list files in dir
-    print (fastq_dir)
+    try:
+        print('Dump fastq.\n')
+        # run_cmd
+        dump_fastq_from_sra(path_, fastq_dir)
+        # os.listdir(fastq_dir) list files in dir
+        print (fastq_dir)
+    except Exception as e:
+        error_class = e.__class__.__name__  # 取得錯誤類型
+        detail = e.args[0]  # 取得詳細內容
+        cl, exc, tb = sys.exc_info()  # 取得Call Stack
+        lastCallStack = traceback.extract_tb(tb)[-1]  # 取得Call Stack的最後一筆資料
+        fileName = lastCallStack[0]  # 取得發生的檔案名稱
+        lineNum = lastCallStack[1]  # 取得發生的行號
+        funcName = lastCallStack[2]  # 取得發生的函數名稱
+        errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class,
+                                                               detail)
+        print(errMsg)
 
     ###
     #print("dump du-sh\n")
