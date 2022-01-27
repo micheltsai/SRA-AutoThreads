282ac9f6ea55bbb6fe5b91fdfd4a1e6cb18efe46
Cai Yi-Xiu <sj985517@gmail.com>
2022-01-27 17:49:41 +0800

    modify Download_sbatch.py

--- Download_sbatch.py
+++ Download_sbatch.py
@@ -25,11 +25,11 @@ def getProgramTime():
         print(str)
         print(len(str))
         for x in range(1,len(str)-1):
-            str=str[x].strip().split(" ")
+            str=str[x].strip().split("   ")
             str=[x for x in str if x!='']
             print(str)
             programID=str[0]
-            time=str[2]
+            time=str[1]
             time=time.split(":")
             if len(time)>2:
                 if int(time[0])>1:
