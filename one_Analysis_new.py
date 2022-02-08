907ab154034d8ec9ea3dd0b575cc0be2c49b3de2
Cai Yi-Xiu <sj985517@gmail.com>
2022-02-08 13:21:57 +0800

    modify Download_sbatch.py

--- one_Analysis_new.py
+++ one_Analysis_new.py
@@ -168,7 +168,7 @@ def Assembled(x,_outdir,sra_dir,ass_dir,assemble_dir,fastq_dir,thread,gsize,star
         #run_cmd(rmsra_cmd)
 
 
-def QualityCheck(sra_id,_outdir,genome_Path,thread,gsize,start):
+def QualityCheck(sra_id,_outdir,outdir,genome_Path,thread,gsize,start):
     ###
     #print("Qualitycheck_start du-sh\n")
     #utils_.run_cmd("du ./SRAtest -sh")
@@ -263,7 +263,7 @@ def QualityCheck(sra_id,_outdir,genome_Path,thread,gsize,start):
         else:
             not_num += 1
     # num =0
-    QC_error=os.path.join(_outdir,"nofillQC.txt")
+    QC_error=os.path.join(outdir,"nofillQC.txt")
     if num==0:
         with open(QC_error, "a+") as f:
             f.write("{}: all ANI value < 95\n".format(sra_id))
@@ -754,7 +754,7 @@ def Analysis(sra_id,input,target_ref,anoutdir,_outdir,thread,gsize,start):
         f.write("Run {} is ok.\n".format(inId))
     return 0
 
-def SRA_Analysis(sra_id,sra_dir,ass_dir,fastq_dir,assemble_dir,_outdir,thread,gsize,start,sra_num_):
+def SRA_Analysis(sra_id,sra_dir,ass_dir,fastq_dir,assemble_dir,_outdir,thread,gsize,start,sra_num_,outdir):
     SRA_start=time.time()
     try:
 
@@ -767,7 +767,7 @@ def SRA_Analysis(sra_id,sra_dir,ass_dir,fastq_dir,assemble_dir,_outdir,thread,gs
 
         genome = os.path.join(ass_dir, "{}_contig.fa".format(sra_id))
         #sys.exit()
-        targetPath=QualityCheck(sra_id,_outdir,genome,thread,gsize,start)
+        targetPath=QualityCheck(sra_id,_outdir,outdir,genome,thread,gsize,start)
         time.sleep(1)
         with open("./checkQC.txt","a+") as f:
             f.write("Run {} is ok.\n".format(sra_id))
@@ -873,7 +873,7 @@ if __name__ == '__main__':
     print(sra_index)
     print(sra_id)
 
-    SRA_Analysis(sra_id, sra_dir, ass_dir, fastq_dir, assemble_dir, new_outdir, thread, gsize, start, sra_num_)
+    SRA_Analysis(sra_id, sra_dir, ass_dir, fastq_dir, assemble_dir, new_outdir, thread, gsize, start, sra_num_, outdir)
 
     #shutil.rmtree(sra_dir)
    #utils_.mkdir_join(fastq_dir)
